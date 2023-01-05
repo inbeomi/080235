1d5e2f4b666ec2e3764fce052a41d157fcc97f9c
Inbeom Seo <92137358+inbeomi@users.noreply.github.com>
2023-01-05 22:01:43 +0900

    Update Better way4.py

--- Chapter1/Better way4.py
+++ Chapter1/Better way4.py
@@ -4,7 +4,7 @@
 
 a = 0b10111011
 b = 0xc5f
-print('이진수: %d, 십육진수: %d' % (a, b))
+print('이진수: %d, 십육진수: %d' % (a, b)) # %d : 십진수로 표시할 때 사용
 
 key = 'my_var'
 value = 1.234
@@ -12,7 +12,7 @@ formatted = '%-10s = %.2f' % (key, value)
 print(formatted)
 
 # 오류가 나는 부분. 오류를 보고 싶으면 커멘트를 해제할것
-#reordered_tuple = '%-10s = %.2f' % (value, key)  # TypeError
+#reordered_tuple = '%-10s = %.2f' % (value, key)  # TypeError : % 타입 순서와 데이터 순서가 다르면 데이터의 타입 변환이 불가능하다. 
 
 # 오류가 나는 부분. 오류를 보고 싶으면 커멘트를 해제할것
 #reordered_string = '%.2f = %-10s' % (key, value) # TypeError
@@ -29,7 +29,7 @@ for i, (item, count) in enumerate(pantry):
 for i, (item, count) in enumerate(pantry):
     print('#%d: %-10s = %d' % (
         i + 1,
-        item.title(),
+        item.title(), # title : 단어의 앞만 대문자로 바꿔주는 함수
         round(count)))
 
 template = '%s는 음식을 좋아해. %s가 요리하는 모습을 봐요.'
@@ -72,7 +72,7 @@ for i, (item, count) in enumerate(pantry):
         'loop': i + 1,
         'item': item.title(),
         'count': round(count),
-    }
+    } # 각 키를 최소 두 번 반복하게 된다. (한 번은 형식 지정자에, 다른 한 번은 딕셔너리 키에) 이는 가독성을 해친다. 
     
     assert before == after
 
@@ -107,21 +107,21 @@ print('*', formatted, '*')
 key = 'my_var'
 value = 1.234
 
-formatted = '{} = {}'.format(key, value)
+formatted = '{} = {}'.format(key, value) # 순서가 같은 위치에 있는 인자를 가리킨다. 
 print(formatted)
 
-formatted = '{:<10} = {:.2f}'.format(key, value)
-print(formatted)
+formatted = '{:<10} = {:.2f}'.format(key, value) # 콜론(:) 뒤에 형식 지정자를 붙여준다. 모든 형식 지정자에 대한 정보를 보고 싶으면 help('FORMATTING') 를 참고하라.
+print(formatted)                                 # {:.2f}는 format(value, '.2f')와도 같다. 
 
-print('%.2f%%' % 12.5)
-print('{} replaces {{}}'.format(1.23))
+print('%.2f%%' % 12.5) # %를 나타내고 싶다면 %%로 이스케이프해야 한다.
+print('{} replaces {{}}'.format(1.23)) # str.format을 사용할 때는 중괄호를 이스케이프해야 한다. 
 
 
-formatted = '{1} = {0}'.format(key, value)
+formatted = '{1} = {0}'.format(key, value) # 중괄호 안에 위치 인덱스를 지정해줄 수도 있다. 
 print(formatted)
 
 name = '철수'
-formatted = '{0}는 음식을 좋아해. {0}가 요리하는 모습을 봐요.'.format(name)
+formatted = '{0}는 음식을 좋아해. {0}가 요리하는 모습을 봐요.'.format(name) # 동일한 위치 인덱스를 여러 번 사용할 수 있다. 
 print(formatted)
 
 for i, (item, count) in enumerate(pantry):
@@ -133,12 +133,12 @@ for i, (item, count) in enumerate(pantry):
     new_style = '#{}: {:<10s} = {}'.format(
         i + 1,
         item.title(),
-        round(count))
+        round(count))   # 이러한 방식도 마찬가지로 가독성은 좋지 못하다. 
         
     assert old_style == new_style
 
-formatted = '첫번째 글자는 {menu[oyster][0]!r}'.format(
-    menu=menu)
+formatted = '첫번째 글자는 {menu[oyster][0]!r}'.format( 
+    menu=menu) # 위와 같이 딕셔너리 키나 리스트 인덱스를 조합해 위치 지정자에 사용하거나 값을 유니코드나 repr 문자열로 변환하는 등의 고급 옵션이 있다. 
 print(formatted)
 
 old_template = (
@@ -161,13 +161,14 @@ new_formatted = new_template.format(
     special='schnitzel',
 )
 
-assert old_formatted == new_formatted
+assert old_formatted == new_formatted # 결국 str.format 메서드는 사용하지 말 것을 권한다. 그러나 여기의 아이디어가 일부 f-문자열의 동작과 유용성을 이해하는 데 도움을 준다. 
 
 #
 # 인터폴레이션 
 #
 
-formatted = f'{key!r:<10} = {value:.2f}'
+# 바이트 문자열 앞에는 b문자를 붙이고, raw 문자열에는 r문자를 붙이는 것처럼 이러한 문자열에는 f문자를 붙인다.
+formatted = f'{key!r:<10} = {value:.2f}'  # !r : 아마도 repr 문자열로 변환하는 것 같다. 
 print(formatted)
 
 f_string = f'{key:<10} = {value:.2f}'
@@ -207,4 +208,4 @@ for i, (item, count) in enumerate(pantry):
 
 places = 3
 number = 1.23456
-print(f'내가 고른 숫자는 {number:.{places}f}')
+print(f'내가 고른 숫자는 {number:.{places}f}') # 형식 옵션값을 하드코딩하는 대신 변수를 사용해 형식 문자열 안에 파라미터화했다. 
