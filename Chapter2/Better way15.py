a8e82a068619d6b2189bcb815085f821b420eb7e
Inbeom Seo <92137358+inbeomi@users.noreply.github.com>
2023-01-07 23:28:47 +0900

    Update Better way15.py

--- Chapter2/Better way15.py
+++ Chapter2/Better way15.py
@@ -18,7 +18,7 @@ def my_func(**kwargs):
     for key, value in kwargs.items():
         print(f'{key} = {value}')
 
-my_func(goose='gosling', kangaroo='joey') # 3.5와 3.7이후가 다름
+my_func(goose='gosling', kangaroo='joey') # 3.5와 3.7이후가 다름 : 3.7 이후에는 키워드 인자가 넣은 순서대로 나오게 된다.
 
 class MyClass:
     def __init__(self):
@@ -26,9 +26,12 @@ class MyClass:
         self.elephant = 'calf'
 
 a = MyClass()
-for key, value in a.__dict__.items():
+for key, value in a.__dict__.items(): # 클래스도 인스턴스 딕셔너리에 dict 타입을 사용하는데 파이썬 3.7 이후로 삽입 순서를 유지하게 된다. 
     print(f'{key} = {value}')
 
+# 이전에는 삽입 순서를 유지해주는 OrderDict 클래스를 활용하곤 했다. 지금은 표준 dict의 동작과 비슷하지만 성능 특성은 다르다.
+# 만약, 키 삽입과 popitem 호출을 자주 처리한다면 표준 dict보다는 OrderDict가 더 낫다. 
+    
 votes = {
     'otter': 1281,
     'polar bear': 587,
@@ -52,7 +55,7 @@ print(winner)
 
 from collections.abc import MutableMapping
 
-class SortedDict(MutableMapping):
+class SortedDict(MutableMapping): # collections.abc 묘듈을 사용해 딕셔너리외 비슷하지만 내용을 알파벳 순서대로 이터레이션해주는 클래스를 새로 정의할 수 있다.
     def __init__(self):
         self.data = {}
 
@@ -75,14 +78,14 @@ class SortedDict(MutableMapping):
         return len(self.data)
 
 
-sorted_ranks = SortedDict()
+sorted_ranks = SortedDict() # SortedDict는 표준 딕셔너리의 프로토콜을 지킨다. 그래서 오류는 나지 않지만 실행 결과는 요구 사항에 맞지 않는다. 
 populate_ranks(votes, sorted_ranks)
 print(sorted_ranks.data)
 winner = get_winner(sorted_ranks)
 print(winner)
 
 def get_winner(ranks):
-    for name, rank in ranks.items():
+    for name, rank in ranks.items(): # 딕셔너리가 어떤 특정 순서로 이터레이션 된다고 생각하지 않고도 원하는 값을 반환하도록 하는 방식. 
         if rank == 1:
             return name
 
@@ -90,17 +93,19 @@ winner = get_winner(sorted_ranks)
 print(winner)
 
 def get_winner(ranks):
-    if not isinstance(ranks, dict):
-        raise TypeError('dict 인스턴스가 필요합니다')
+    if not isinstance(ranks, dict):  # 아니면 표준 딕셔너리 타입으로 들어올 수 있도록 예외를 던지는. 
+        raise TypeError('dict 인스턴스가 필요합니다') # 앞의 것보다 실행 성능은 더 좋을 것이다. 
     return next(iter(ranks))
 
 
-from typing import Dict, MutableMapping
+from typing import Dict, MutableMapping # 타입 어노테이션을 붙이고 mypy 도구를 엄격한 모드(strict)로 실행해서 버그를 최대한 없앤다. 
+                                        # $ python3 -m mypy --strict example.py
+                                        # 이 방식은 적절한 타입의 객체를 사용하지 않았을 대 오류를 발생시킨다. 
 
 def populate_ranks(votes: Dict[str, int],
-                   ranks: Dict[str, int]) -> None:
+                   ranks: Dict[str, int]) -> None: # 아니면 Type Annotation을 사용해서 MutableMapping 인스턴스가 아닌 Dict 인스턴스가 강제되도록 한다.
     names = list(votes.keys())
-    names.sort(key=votes.get, reverse=True)
+    names.sort(key=votes.get, reverse=True) # dict.get을 쓰면 디폴트 값은 None 처리가 되니 dict를 key값으로만 다루기 좋은. 
     for i, name in enumerate(names, 1):
         ranks[name] = i
 
