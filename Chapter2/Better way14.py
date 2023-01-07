d4808e277ff0e4fcbd04c2c91688d7b339f479e2
Inbeom Seo <92137358+inbeomi@users.noreply.github.com>
2023-01-07 21:49:40 +0900

    Update Better way14.py

--- Chapter2/Better way14.py
+++ Chapter2/Better way14.py
@@ -48,30 +48,30 @@ drill = (4, '드릴')
 sander = (4, '연마기')
 assert drill[0] == sander[0] # 무게가 같다
 assert drill[1] < sander[1]  # 알파벳순으로 볼 때 더 작다
-assert drill < sander        # 그러므로 드릴이 더 먼저다
+assert drill < sander        # 그러므로 드릴이 더 먼저다 : 튜플의 원소를 첫 번째부터 차례로 비교하며 순위를 정한다. 
 
-power_tools.sort(key=lambda x: (x.weight, x.name))
+power_tools.sort(key=lambda x: (x.weight, x.name)) # default값은 오름차순이다.  
 print(power_tools)
 
 power_tools.sort(key=lambda x: (x.weight, x.name),
                  reverse=True) # 모든 비교 기준을 내림차순으로 만든다
 print(power_tools)
 
-power_tools.sort(key=lambda x: (-x.weight, x.name))
+power_tools.sort(key=lambda x: (-x.weight, x.name)) # 모든 비교 기준이 동일하게 반영이 되니, 이처럼 -를 붙여서 다르게 반영되도록 한다
 print(power_tools)
 
 # 오류가 나는 부분. 오류를 보고 싶으면 커멘트를 해제할것
-#power_tools.sort(key=lambda x: (x.weight, -x.name),
+#power_tools.sort(key=lambda x: (x.weight, -x.name), # 숫자가 아닌 문자열은 - 처리가 되지 않는다.
 #                 reverse=True)
 
-power_tools.sort(key=lambda x: x.name)   # name 기준 오름차순
+power_tools.sort(key=lambda x: x.name)   # name 기준 오름차순 
 power_tools.sort(key=lambda x: x.weight, # weight 기준 내림차순
                  reverse=True)
 print(power_tools)
 
-power_tools.sort(key=lambda x: x.name)
-print(power_tools)
-
+power_tools.sort(key=lambda x: x.name)   # 만약, key 함수가 반환하는 값이 서로 같은 경우 리스트에 들어 있던 원래 순서를 그대로 유지해준다. 
+print(power_tools)                       # 이는, 같은 리스트에 대해 서로 다른 기준으로 sort를 여러 번 호출해도 된다는 뜻이다. (부호를 바꿀 수 없는 타입이라면 고려할 만하다.)
+                                         # 여러 번 호출하는 방법은 필요할 때만 사용하는 것을 권장한다. 
 power_tools.sort(key=lambda x: x.weight,
                  reverse=True)
 print(power_tools)
